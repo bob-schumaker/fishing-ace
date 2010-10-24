8502c552744c443dda0a0109fdfe2ef9b1231641
Sayclub <Sayclub@7bc41a47-f47b-48d4-a2aa-b73d452b4282>
2010-10-24 03:55:32 +0000

    FishingAce: - koKR update
    git-svn-id: file:///Users/bobschumaker/Repos/svn/fishing-ace/trunk@115 7bc41a47-f47b-48d4-a2aa-b73d452b4282

--- Locales/Locale-koKR.lua
+++ Locales/Locale-koKR.lua
@@ -1,11 +1,11 @@
 ﻿local L = LibStub("AceLocale-3.0"):NewLocale("FishingAce", "koKR", false)
 
 if L then
-	L["on"] = "on"
-	L["off"] = "off"
+	L["on"] = "켬"
+	L["off"] = "끔"
 
 	L["Fishing Ace!"] = "Fishing Ace!"
-	L["Description"] = "Fishing Ace! enables you to fish with a double-click whenever it detects you're using a fishing pole. Settings to enhance your fishing experience can be set below."
+	L["Description"] = "낚싯대 사용을 감지할 경우 Fishing Ace!는 더블 클릭으로 낚시를 편하게 할 수 있도록 도와줍니다.\n당신의 낚시 경험을 향상시키기 위해 아래 설정을 이용하실 수 있습니다."
 
 	L["Auto Loot"] = "자동 획득"
 	L["AutoLootMsg"] = "설정시, Fishing Ace!는 당신이 낚시를 하는 동안 자동으로 전리품을 획득하도록 합니다."
@@ -20,5 +20,5 @@ if L then
 	L["UseActionMsg"] = "설정시, Fishing Ace!는 시전할 행동 단축키을 찾습니다."
 
 	L["LureSkill"] = "사용 효과: 낚싯대에 사용하면 %d분 동안 낚시 숙련도가 (%d)만큼 증가합니다."
-	
+
 end
