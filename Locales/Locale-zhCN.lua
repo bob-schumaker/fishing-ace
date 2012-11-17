7615fd321c52becf38f7358419dd3c391aabed95
sutorix <sutorix@7bc41a47-f47b-48d4-a2aa-b73d452b4282>
2012-11-17 02:13:03 +0000

    FishingAce: Missing parenthesis in @@ expansion
    git-svn-id: file:///Users/bobschumaker/Repos/svn/fishing-ace/trunk@203 7bc41a47-f47b-48d4-a2aa-b73d452b4282

--- Locales/Locale-zhCN.lua
+++ Locales/Locale-zhCN.lua
@@ -3,5 +3,5 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("FishingAce", "zhCN", false)
 
 if L then
-@localization(locale="zhCN", format="lua_additive_table"), handle-unlocalized="english"@
+@localization(locale="zhCN", format="lua_additive_table"), handle-unlocalized="english")@
 end
